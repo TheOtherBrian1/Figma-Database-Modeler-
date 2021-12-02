168db758b9cc0403073987ed220080bdc6ea009b
Brian Brennglass <>
2021-12-01 21:02:46 -0500

    saving before making major changes to VisualEditor

--- src/ui.tsx
+++ src/ui.tsx
@@ -2,10 +2,12 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import VisualEditor from './components/VisualEditor/VisualEditor'
 import Controller from './components/Controller/Controller'
+import ModTable from './components/ModTable/ModTable'
 function App (){
     return (
       <div>
         <Controller />
+        <ModTable />
         <VisualEditor />
       </div>
     )
